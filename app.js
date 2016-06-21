066f816b27a40d5b041b0e7d44b4c8e23609431f
Ryszard Perkowski <usultis@gmail.com>
2016-06-21 14:00:21 +0000

    leaving just http access

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
 var express = require('express')
 var app = express()
-var config = require('./config')
-
-/* airbrake */
-if ( config.USE_AIRBRAKE ){
-  var airbrake = require('airbrake').createClient(config.AIRBRAKE_API_KEY, 'production');
-  airbrake.serviceHost = config.AIRBRAKE_SERVICE_HOST;
-  app.use(airbrake.expressHandler());
-}
-/**/
 
 var gpio = require("pi-gpio");
-var pin = 11;
-var relayOpen = false;
-
-// require('coffee-script/register');
-var Slack = require('slack-client');
-
-var token = config.SLACK_TOKEN, // Add a bot at https://my.slack.com/services/new/bot and copy the token here.
-    autoReconnect = true,
-    autoMark = true;
-var slack = new Slack(token, autoReconnect, autoMark);
-slack.on('open', function() {
-  var unreads = slack.getUnreadCount();
-  console.log('Welcome to Slack. You are @%s of %s', slack.self.name, slack.team.name);
-});
-
-
-/* gpio */
+var pin = 7;
 gpio.close(pin);                     // Close pin 16
 
 gpio.open(pin, "output", function(err) {     // Open pin 16 for output
@@ -47,64 +22,6 @@ var server = app.listen(3000, function () {
 
 })
 
-/* Slack */
-slack.on('message', function(message) {
-  var type = message.type,
-      channel = slack.getChannelGroupOrDMByID(message.channel),
-      user = slack.getUserByID(message.user),
-      time = message.ts,
-      text = message.text,
-      response = '';
-
-  if (user === undefined){
-    /* skip channel messages(skip the messages which are not sent to sesame directly) */
-    return true;
-  }
-  console.log('Received: %s %s @%s %s "%s"', type, (channel.is_channel ? '#' : '') + channel.name, user.name, time, text);
-
-  if (type === 'message' && (user.name === 'neko' || user.name === 'sakir')) {
-    if (text === 'open' || (text.indexOf('open ') === 0)){
-      
-      if(text === 'open'){
-        doorTimeout = 1;
-      }else{
-        doorTimeout = parseInt( text.replace("open ", "") );
-      }
-
-      if(doorTimeout > 10){
-        doorTimeout = 1;
-      }
-
-      if (doorTimeout == 0){
-        closeRelay();
-      }else{
-        openDoor(doorTimeout);
-      }
-      response = 'ok';
-      channel.send(response);
-      console.log('@%s responded with "%s"', slack.self.name, response);
-    }
-  }
-});
-
-slack.on('error', function(error) {
-  console.error('Error: %s', error);
-});
-
-slack.login();
-/* /slack */
-function openDoor(doorTimeout){
-  doorTimeout = doorTimeout || 1;
-  if(!relayOpen){
-    relayOpen = true;
-    console.log('open called');
-    gpio.write(pin, 0, function() {});
-
-      setTimeout(function() {
-          closeRelay();
-      }, (doorTimeout * 1000));
-  }
-}
 
 function closeRelay(){
   gpio.write(pin, 1, function(){});
